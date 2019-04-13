69bdaf97aa6a3df192fdc132f5a47944884f8472
jackground <jackground@icloud.com>
2019-04-13 15:40:27 -0700

    changed function type of door functions to bool. added pickDoor and matchDoor functions. added variable bool go in main function.

--- main.cpp
+++ main.cpp
@@ -30,49 +30,74 @@ void drawPicture();
 
 void pictureRoom();
 
-bool exitRoom(int bananas,              //
+void exitRoom(int bananas,              //
               int oranges);             //
 
-void doorN(bool alignment,              //
+bool doorN(bool alignment,              //
            int& bananas,                //
            int& oranges,                //
            string monsterName);         //
 
-void doorE(bool alignment,              //
+bool doorE(bool alignment,              //
            int& bananas,                //
            int& oranges,                //
            string monsterName);         //
 
-void doorS(bool alignment,              //
+bool doorS(bool alignment,              //
            int& bananas,                //
            int& oranges);               //
 
-void doorW(bool alignment,              //
+bool doorW(bool alignment,              //
            int& bananas,                //
            int& oranges);               //
+
+void outputUpdate (string playerName,
+                   int bananas,
+                   int oranges);
+
+void pickDoor (char& door);
+void matchDoor (char& door,
+                bool& alignment,
+                int& bananas,
+                int& oranges,
+                string monsterName,
+                bool& go);
 
 
 int main()
 {
+    srand(500);
+
     // Variables
     int bananas;
     int oranges;
     string playerName;
     string monsterName;
     bool alignment;
+    char door;
+    bool go;
 
+    // Variable Initialization
+    bananas = 5;
+    oranges = 3;
+    go = 1;
 
-// Testing
-//    bananas = 3;
-//    oranges = 2;
-//    monsterName = "penisbreath";
-//    alignment = 0;
+    cout << "Please enter your name: ";
+    cin >> playerName;
+    cout << endl;
 
-//    doorN(alignment, bananas, oranges, monsterName);
+    cout << "Name your scariest monster: ";
+    cin >> monsterName;
+    cout << endl;
 
-//    cout << bananas << endl;
-//    cout << oranges << endl;
+    do
+    {
+        outputUpdate(playerName, bananas, oranges);
+        alignment = rand() % 2;
+        pickDoor(door);
+        matchDoor(door, alignment, bananas, oranges, monsterName, go);
 
+    } while (go != 0);
 
     return 0;
 }
@@ -125,16 +150,15 @@ void pictureRoom()
     drawPicture();
 }
 
-bool exitRoom(int bananas,          //
+void exitRoom(int bananas,          //
               int oranges)          //
 {
     cout << "You found the exit!" << endl;
     cout << "Your score is " << bananas + oranges << " (" << bananas
          << " bananas and " << oranges << " oranges)." << endl;
-    return 0;
 }
 
-void doorN(bool alignment,          //
+bool doorN(bool alignment,          //
            int& bananas,            //
            int& oranges,            //
            string monsterName)      //
@@ -142,14 +166,16 @@ void doorN(bool alignment,          //
     if (alignment == 0)
     {
         monsterRoom(bananas, oranges, monsterName);
+        return 1;
     }
     else
     {
         exitRoom(bananas, oranges);
+        return 0;
     }
 }
 
-void doorE(bool alignment,          //
+bool doorE(bool alignment,          //
            int& bananas,            //
            int& oranges,            //
            string monsterName)      //
@@ -157,43 +183,93 @@ void doorE(bool alignment,          //
     if (alignment == 0)
     {
         genieRoom(bananas, oranges);
+        return 1;
     }
     else
     {
         monsterRoom(bananas, oranges, monsterName);
+        return 1;
     }
 }
 
-void doorS(bool alignment,          //
+bool doorS(bool alignment,          //
            int& bananas,            //
            int& oranges)            //
 {
     if (alignment == 0)
     {
         pictureRoom();
+        return 1;
     }
     else
     {
         genieRoom(bananas, oranges);
+        return 1;
     }
 }
 
-void doorW(bool alignment,          //
+bool doorW(bool alignment,          //
            int& bananas,            //
            int& oranges)            //
 {
     if (alignment == 0)
     {
         exitRoom(bananas, oranges);
+        return 0;
     }
     else
     {
         pictureRoom();
+        return 1;
     }
 }
 
+void outputUpdate (string playerName,
+                   int bananas,
+                   int oranges)
+{
+    cout << playerName << ", you are in a room with 4 doors." << endl;
+    cout << "You are carrying " << bananas << " bananas and " << oranges
+         << " oranges." << endl;
+    cout << endl;
+}
 
+void pickDoor (char& door)
+{
+    do
+    {
+    cout << "Pick a door to enter by typing the direction it is in (N/E/S/W): ";
+    cin >> door;
+    cout << endl;
 
+    }
+    while (door != 'N' && door != 'S' && door != 'E' && door != 'W');
+}
+
+void matchDoor (char& door,
+                bool& alignment,
+                int& bananas,
+                int& oranges,
+                string monsterName,
+                bool& go)
+{
+    if (door == 'N')
+    {
+        go = doorN(alignment, bananas, oranges, monsterName);
+    }
+    else if (door == 'E')
+    {
+        go = doorE(alignment, bananas, oranges, monsterName);
+    }
+    else if (door == 'S')
+    {
+        go = doorS(alignment, bananas, oranges);
+    }
+    else if (door == 'W')
+    {
+        go = doorW(alignment, bananas, oranges);
+    }
+}
 
 
 
